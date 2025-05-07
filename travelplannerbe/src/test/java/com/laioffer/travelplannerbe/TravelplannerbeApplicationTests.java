261cc64a3fcfc2e9fa3202f46e3141c41050b45d
Marcus-Zhang-1005 <puddingsai0725@gmail.com>
2025-05-06 17:53:32 -0700

    Converted travelplannerfe from submodule to normal directory

---
+++ travelplannerbe/src/test/java/com/laioffer/travelplannerbe/TravelplannerbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laioffer.travelplannerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelplannerbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ travelplannerfe/README.md
@@ -0,0 +1,3 @@
+Let's test your connection with Git repo.
+
+Yuqi Zhang 04/29/25
