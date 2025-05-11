229a16e37344c39d6cdf5c34787a6e9c8f49c255
Marcus-Zhang-1005 <puddingsai0725@gmail.com>
2025-05-11 10:37:18 -0700

    fe login & userhomepage
    # Conflicts:
    #	travelplannerbe/src/main/java/com/laioffer/travelplannerbe/AppConfig.java

---
+++ travelplannerbe/src/main/java/com/laioffer/travelplannerbe/AppConfig.java
@@ -0,0 +1,26 @@
+package com.laioffer.travelplannerbe;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers( "/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+}
