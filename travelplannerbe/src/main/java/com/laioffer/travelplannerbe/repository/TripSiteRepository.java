f5bc68a8bfbedbe7e52c4d5e4d4d875500933b81
Marcus-Zhang-1005 <puddingsai0725@gmail.com>
2025-05-11 11:25:47 -0700

    debug database again

--- travelplannerbe/src/main/java/com/laioffer/travelplannerbe/repository/TripSiteRepository.java
+++ travelplannerbe/src/main/java/com/laioffer/travelplannerbe/repository/TripSiteRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
 
 public interface TripSiteRepository extends JpaRepository<TripSiteEntity, TripSiteKey> {
-    
+
 }
